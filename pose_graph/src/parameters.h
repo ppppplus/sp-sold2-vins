8dd676f6df712d98febff3f2c458770a3e65569f
ppppplus <ppppplustjh@gmail.com>
2023-12-08 14:00:39 +0000

    add pose_graph&test1

--- pose_graph/src/parameters.h
+++ pose_graph/src/parameters.h
@@ -21,7 +21,10 @@ extern std::string BRIEF_PATTERN_FILE;
 extern std::string POSE_GRAPH_SAVE_PATH;
 extern int ROW;
 extern int COL;
+extern std::string VINS_FOLDER_PATH;
 extern std::string VINS_RESULT_PATH;
+extern std::string DATASET_NAME;
+extern std::string TIME_STAMP;
 extern int DEBUG_IMAGE;
 extern int FAST_RELOCALIZATION;
 
